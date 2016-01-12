418cd63663b64bf2a88e601bd0d80bf8fffae087
Beth Fraser <bethanyfraser@live.co.uk>
2016-01-12 14:19:46 +0000

    Days controller successfully added to view lineup by day in mobile size.

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,19 @@
+class DaysController < ApplicationController
+
+
+  def index
+  end
+
+  def show
+    event = Event.find(1)
+    timeslots = Timeslot.order(:venue_id).all
+    timeslots = timeslots.select { |t| t.act.event.id == 1}
+
+    @day = timeslots.select { |t| t.start_time.day == params[:id].to_i}
+    @start_time = @day.first.start_time
+    @venues = @day.group_by { |t| t.venue_id }.values
+
+    @booking = Booking.new
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :keywords
+  resources :days
 
   root to: 'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
