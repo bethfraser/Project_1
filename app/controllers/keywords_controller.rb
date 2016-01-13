08033db382af70c7801c38e5fe68656a87b6ef12
Beth Fraser <bethanyfraser@live.co.uk>
2016-01-13 11:44:39 +0000

    Added markdown gem for act profile, fixed bug on user schedule.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,4 +1,7 @@
 class KeywordsController < ApplicationController
+
+  load_and_authorize_resource
+  
   def index
     @keywords = Keyword.all
   end
--- app/models/act.rb
+++ app/models/act.rb
@@ -8,4 +8,11 @@ has_many :timeslots, dependent: :destroy
 has_many :venues, through: :timeslots
 
 has_and_belongs_to_many :keywords
+
+def html
+  renderer = Redcarpet::Render::HTML.new(render_options = {})
+  markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+  markdown.render(self.description).html_safe
+end
+
 end
